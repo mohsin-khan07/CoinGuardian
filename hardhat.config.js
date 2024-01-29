fae77a584f1dc8591135e95b3222cee708084ba7
Mohsin Khan <mkhan6720@gmail.com>
2024-01-29 20:53:41 +0530

    basic configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: import.meta.env.VITE_ALCHEMY_SEP_URL,
+      accounts: [import.meta.env.VITE_PRIVATE_KEY],
+    },
+  },
 };
