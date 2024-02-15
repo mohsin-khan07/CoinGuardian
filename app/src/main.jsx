a4616f9dc0fb5a64187bb2f8ef92763fc9f6ab23
Mohsin Khan <mkhan6720@gmail.com>
2024-02-15 13:31:33 +0530

    mobile optimized

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,18 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-if (!window.ethereum) {
-  root.render(
-    <React.StrictMode>
-      You need to install a browser wallet to build the escrow dapp
-    </React.StrictMode>
-  );
-} else {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
